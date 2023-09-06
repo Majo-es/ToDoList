598506957ac13d9f2c1274629598bc7aa778653e
Maria Jose <43044338+Majo-es@users.noreply.github.com>
2023-09-05 19:00:38 -0600

    Update ContentView.swift

--- Views/ContentView.swift
+++ Views/ContentView.swift
@@ -9,6 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     
+    //@EnvironmentObject: property wrapper type for an observable object that a parent or ancestor view supplies.
+    @EnvironmentObject var realManager: RealmManager
+    
+    //@StateObject: property wrapper type that instantiates an observable object.
     @StateObject var realmManager = RealmManager ()
     
     //private key = is only accessible inside the content view
@@ -25,11 +29,12 @@ struct ContentView: View {
                     showAddTaskView.toggle()
                 }
         }
+        //Sheet: present a modal view to the user when a Boolean value provided is true.
         .sheet(isPresented: $showAddTaskView){
             AddTaskView()
                 .environmentObject(realmManager)
         }
-        
+        //Frame: method to specify a fixed size for a view’s width, height, or both.
         .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottom)
         .background(Color(hue: 0.709, saturation:0.085, brightness:0.976))
     }
@@ -37,6 +42,7 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
+        //contentview: content view of the cell object
         ContentView()
     }
 }
